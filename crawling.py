442eddbcca72943248f8d13d93aa7e1528239979
zena-H <63337285+zena-H@users.noreply.github.com>
2021-12-06 15:38:18 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -11,6 +11,7 @@ from generate import Generate
 
 class Topic:
     def __init__(self):
+        model_url = './etri_et5'
         self.headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"}
         self.generate = Generate(model_url)
 
